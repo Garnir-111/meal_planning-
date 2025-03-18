1e73319603cd7e26e2bc77d9f4998fefc1b437fa
jbfawcett24 <jbfawcett24@gmail.com>
2025-03-18 10:57:56 -0600

    filter basic functionality

--- testsite/filters.js
+++ testsite/filters.js
@@ -2,10 +2,14 @@ const priceOutput = document.querySelector("#currentPriceRange");
 const minPrice = document.querySelector("#minPrice");
 const maxPrice = document.querySelector("#maxPrice");
 
-function setPriceRange()
-{
+function setPriceRange() {
     console.log("called");
+    if (maxPrice.value !== minPrice.value) {
+        maxPrice.min = minPrice.value;
+        minPrice.max = maxPrice.value;
+    }
     priceOutput.innerText = `${minPrice.value} - ${maxPrice.value}`;
 }
 
-minPrice.addEventListener("mouseup", setPriceRange);
+minPrice.addEventListener("input", setPriceRange);
+maxPrice.addEventListener("input", setPriceRange);
